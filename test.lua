565737a204ae7f56952b0f019339b317510fc80b
aratonstilts <114688812+aratonstilts@users.noreply.github.com>
2024-02-13 00:35:23 -0500

    Update test.lua

--- test.lua
+++ test.lua
@@ -5,12 +5,13 @@ local fishingGame = instances:WaitForChild("FishingGame")
 local gameBar = fishingGame:WaitForChild("GameBar")
 
 local camera = workspace.CurrentCamera
+local VIP = game:GetService("VirtualInputManager")
 
 local function clickScreen()
-  local screenSize = camera.ViewportSize
-	VirtualInputManager:SendTouchEvent(1, 0, screenSize.X/2, screenSize.Y/2)
-  task.wait()
-  VirtualInputManager:SendTouchEvent(1, 2, screenSize.X/2, screenSize.Y/2)
+  	local screenSize = camera.ViewportSize
+	VIP:SendTouchEvent(1, 0, screenSize.X/2, screenSize.Y/2)
+  	task.wait()
+  	VIP:SendTouchEvent(1, 2, screenSize.X/2, screenSize.Y/2)
 end
 
 --while Toggled58 == true do
