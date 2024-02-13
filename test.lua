5ef7cfbebdf0fc27e9486a68e66f64f8f8ecff9a
aratonstilts <114688812+aratonstilts@users.noreply.github.com>
2024-02-12 19:19:13 -0500

    Create test.lua

---
+++ test.lua
@@ -0,0 +1,23 @@
+local Player = game:GetService("Players").LocalPlayer
+local playerGUI = Player.PlayerGui
+local instances = playerGui:WaitForChild("_INSTANCES")
+local fishingGame = instances:WaitForChild("FishingGame")
+local gameBar = fishingGame:WaitForChild("GameBar")
+
+local function clickScreen()
+  local screenSize = Camera.ViewportSize
+	VirtualInputManager:SendTouchEvent(1, 0, screenSize.X/2, screenSize.Y/2)
+  task.wait()
+  VirtualInputManager:SendTouchEvent(1, 2, screenSize.X/2, screenSize.Y/2)
+end
+
+--while Toggled58 == true do
+	
+  v = 0
+  clickScreen()
+           
+	task.wait(2)
+    
+	repeat clickScreen() task.wait() until gameBar.Visible == false
+	
+--end
